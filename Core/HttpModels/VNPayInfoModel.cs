198655f2a623234a4fc7c6014ccaae68818f762b
Collin Phan <collincomms+github@gmail.com>
2024-06-06 08:27:47 +0700

    Adding VNPay intergration
    Simple sample code for integrating vnpay payment gateway.

---
+++ Core/HttpModels/VNPayInfoModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.HttpModels
+{
+    public class VNPayInfoModel
+    {
+        public string amount { get; set; } = string.Empty;
+        public string info { get; set; } = string.Empty;
+        public string order_info { get; set; } = string.Empty;
+    }
+}
