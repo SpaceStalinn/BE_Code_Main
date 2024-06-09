2c0aa350c3e410daa27848ed156abfb89ddd6822
SpaceStalinn <125347214+SpaceStalinn@users.noreply.github.com>
2024-06-09 17:09:49 +0700

    Xóa thông tin cá nhân trong appsetting.json, sửa hàm lấy connectiong string trong DentalClinicPlatformContext

--- Repositories/Models/DentalClinicPlatformContext.cs
+++ Repositories/Models/DentalClinicPlatformContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Repositories.Models;
 
@@ -48,8 +49,29 @@ public partial class DentalClinicPlatformContext : DbContext
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("server=localhost;database=DentalClinicPlatform;user=sa;password=2512004quocbao;Encrypt=True;TrustServerCertificate=True;Trusted_Connection=True");
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer(GetConnectionString());
+        }
+    }
+
+    private string GetConnectionString()
+    {
+        IConfiguration config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", true, true)
+            .Build();
+
+        var connectionString = config.GetConnectionString("Database");
+
+        if (connectionString == null)
+        {
+            throw new Exception("Connection string not found! Please check your configuration files.");
+        }
+
+        return connectionString;
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
